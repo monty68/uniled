b76ba903a1b81fc726dc775644658ff65c9fbf57
monty68 <4341648+monty68@users.noreply.github.com>
2023-12-02 09:00:26 +0000

    Update models db with new devices

--- custom_components/uniled/lib/ble/models.py
+++ custom_components/uniled/lib/ble/models.py
@@ -2,10 +2,10 @@
 from typing import Final
 from .led_chord import SP107E
 from .led_hue import SP110E
-# from .banlanx2 import
-from .banlanx3 import SP613E, SP614E
 from .banlanx_60x import SP601E, SP602E, SP608E
 from .banlanx_6xx import SP6XXE
+from .banlanx2 import SP611E, SP617E, SP620E, SP621E
+from .banlanx3 import SP613E, SP614E, SP623E, SP624E
 
 
 ##
@@ -19,9 +19,15 @@ UNILED_BLE_MODELS: Final = [
     # SP602E,
     # SP608E,
     ######
-    #SP611E,
-    #SP617E,
+    SP611E,
+    SP617E,
+    SP620E,
+    SP621E,
+    SP623E,
+    SP624E,
+    ######
     SP613E,
     SP614E,
+    ######
     SP6XXE,
 ]
