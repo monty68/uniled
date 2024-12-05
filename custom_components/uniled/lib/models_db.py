17c5366d733f4a7f103d57d62e18139b0f1f9955
monty68 <4341648+monty68@users.noreply.github.com>
2024-12-05 08:12:49 +0000

    Add back in BLE models to master db

--- custom_components/uniled/lib/models_db.py
+++ custom_components/uniled/lib/models_db.py
@@ -2,12 +2,12 @@
 from typing import Final
 from .const import UNILED_TRANSPORT_NET, UNILED_TRANSPORT_BLE
 from .net.models import UNILED_NET_MODELS
-#from .ble.models import UNILED_BLE_MODELS
+from .ble.models import UNILED_BLE_MODELS
 
 ##
 ## All Supported Models
 ##
 UNILED_MODELS: Final = {
     UNILED_TRANSPORT_NET: UNILED_NET_MODELS,
-    #UNILED_TRANSPORT_BLE: UNILED_BLE_MODELS,
+    UNILED_TRANSPORT_BLE: UNILED_BLE_MODELS,
 }
