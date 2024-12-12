7c74f15256d6542619f8d367eed2b67e45350e24
monty68 <4341648+monty68@users.noreply.github.com>
2024-12-12 08:35:34 +0000

    Code tidy and correct logging for devices

--- custom_components/uniled/lib/model.py
+++ custom_components/uniled/lib/model.py
@@ -1,14 +1,14 @@
 """UniLED Base Model."""
 
 from __future__ import annotations
-from dataclasses import dataclass
+
 from abc import abstractmethod
+from dataclasses import dataclass
+import logging
 from typing import Any, NewType
 
 from .channel import UniledChannel
 from .chips import UniledChips
-
-import logging
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,28 +30,28 @@ class UniledModel(UniledChips):
 
     @abstractmethod
     def build_state_query(self, device: UniledDevice) -> bytearray | None:
-        """Build a state query message"""
+        """Build a state query message."""
 
     def build_on_connect(self, device: UniledDevice) -> list[bytearray] | None:
-        """Build state query message"""
+        """Build state query message."""
         return None
 
     def build_command(
         self, device: Any, channel: UniledChannel, attr: str, value: any
     ) -> list[bytearray]:
-        """Build supported command"""
+        """Build supported command."""
         if channel.status.has(attr):
             if channel.get(attr, value) == value:
                 _LOGGER.debug(
-                    "%s: Channel %s, command: %s ignored, as no state changed.",
-                    self.model_name,
+                    "%s: Channel %s, command: %s ignored, as no state changed",
+                    device.name,
                     channel.number,
                     attr,
                 )
                 return []
             _LOGGER.debug(
                 "%s: Channel %s, command: %s = %s (%s)",
-                self.model_name,
+                device.name,
                 channel.number,
                 attr,
                 value,
@@ -63,9 +63,9 @@ class UniledModel(UniledChips):
                 try:
                     if commands := command_builder(device, channel, value):
                         channel.set(attr, value)
-                    return commands
-                except Exception as ex:
-                    _LOGGER.warn(
+                    return commands  # noqa: TRY300
+                except Exception as ex:  # noqa: BLE001
+                    _LOGGER.warning(
                         "%s: Channel %s, command %s generated an exception: %s",
                         self.model_name,
                         channel.number,
@@ -74,15 +74,15 @@ class UniledModel(UniledChips):
                     )
                     return []
             _LOGGER.warning(
-                "%s: Channel %s, command method '%s' not found.",
-                self.model_name,
+                "%s: Channel %s, command method '%s' not found",
+                device.name,
                 channel.number,
                 command_method,
             )
         else:
             _LOGGER.debug(
                 "%s: Channel %s, command: %s ignored, no matching status attribute",
-                self.model_name,
+                device.name,
                 channel.number,
                 attr,
             )
@@ -91,7 +91,7 @@ class UniledModel(UniledChips):
     def build_multi_commands(
         self, device: Any, channel: UniledChannel, **kwargs
     ) -> list[bytearray]:
-        """Build multiple supported command(s)"""
+        """Build multiple supported command(s)."""
         multi = []
         for attr, value in kwargs.items():
             commands = self.build_command(device, channel, attr, value)
@@ -105,12 +105,12 @@ class UniledModel(UniledChips):
     def fetch_attribute_list(
         self, device: Any, channel: UniledChannel, attr: str
     ) -> list:
-        """Return a list of attribute options"""
+        """Return a list of attribute options."""
         list_method = f"fetch_{attr}_list"
         list_fetcher = getattr(self, list_method, None)
         if callable(list_fetcher):
             return list_fetcher(device, channel)
         _LOGGER.warning(
-            "%s: Fetch list method '%s' not found.", self.model_name, list_method
+            "%s: Fetch list method '%s' not found", device.name, list_method
         )
         return []
